80e88201dab2b5bd6a817a71fff7ff3113003222
darwin <darwin931369866@gmail.com>
2022-01-24 17:11:01 +0800

    简单的按键逻辑

--- src/Wizard-button/logic.py
+++ src/Wizard-button/logic.py
@@ -1,3 +1,7 @@
+import time
+
+import keyboard
+import pyautogui
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from main import Ui_MainWindow
 
@@ -12,5 +16,14 @@ class mainUI(QMainWindow,Ui_MainWindow):
     def onClicked(self):
         if(self.pushButton.text()=="准备完毕"):
             self.pushButton.setText("运行中")
+            self.keyboardRunner()
         else:
             self.pushButton.setText("准备完毕")
+
+    def keyboardRunner(self):
+        print(self.lineEdit_8.text())
+        print(self.comboBox.currentText())
+        keyboard.add_hotkey(self.lineEdit_8.text(), self.on_space)
+
+    def on_space(self):
+        pyautogui.press(self.comboBox.currentText(),presses=10000000, interval=float(self.lineEdit.text())/1000)
